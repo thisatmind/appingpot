f0a23414a6e8add07eafd9b7f5c6b8da1eb5a29a
Patrick <nasuk0610@gmail.com>
2016-09-17 09:01:22 +0900

    update tracker

--- app/src/main/java/com/thisatmind/appingpot/appingpot/MainActivity.java
+++ app/src/main/java/com/thisatmind/appingpot/appingpot/MainActivity.java
@@ -1,7 +1,13 @@
 package com.thisatmind.appingpot.appingpot;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.thisatmind.appingpot.appingpot.tracker.Tracker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Tracker.getUsageStatsList(this) == null){
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
+        Tracker.getUsageEvents(this);
+//        Tracker.printfUsageStats(Tracker.getUsageStatsList(this));
+
     }
+
 }
--- app/src/main/java/com/thisatmind/appingpot/appingpot/tracker/Tracker.java
+++ app/src/main/java/com/thisatmind/appingpot/appingpot/tracker/Tracker.java
@@ -1,18 +1,26 @@
 package com.thisatmind.appingpot.appingpot.tracker;
 
 import android.app.Activity;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Patrick on 2016-09-17.
  */
 public class Tracker {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy HH:mm:ss", Locale.KOREA);
+
+    public static final String TAG = Tracker.class.getSimpleName();
 
     public List<ApplicationInfo> getInstalledAppList(Activity activity){
 
@@ -34,7 +42,48 @@ public class Tracker {
         return packages;
     }
 
-    public void getStats(Context context){
+    public static void getUsageEvents(Context context){
         UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.MONTH, -1);
+        long startTime = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Range start : " + dateFormat.format(startTime));
+        Log.d(TAG, "Range end : " + dateFormat.format(endTime));
+
+        UsageEvents uEvents = usm.queryEvents(startTime, endTime);
+        UsageEvents.Event e = new UsageEvents.Event();
+
+        while(uEvents.hasNextEvent()){
+            uEvents.getNextEvent(e);
+
+            if(e != null){
+                Log.d(TAG, e.getEventType() + "/ Event : " + e.getPackageName() + " / " + dateFormat.format(e.getTimeStamp()));
+            }
+        }
+
     }
+
+    public static List<UsageStats> getUsageStatsList(Context context){
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                0, System.currentTimeMillis());
+        return usageStatsList;
+    }
+
+    public static void printfUsageStats(List<UsageStats> usageStatsList){
+        for( UsageStats u : usageStatsList ){
+            Log.d(TAG, "pkg : " + u.getPackageName() + " / " + "ForegroundTime : "
+                + dateFormat.format(u.getTotalTimeInForeground()));
+            Log.d(TAG, "Pkg: " + u.getPackageName() + "\t" + "Start Time : "
+                    + dateFormat.format(u.getFirstTimeStamp()));
+            Log.d(TAG, "Pkg: " + u.getPackageName() + "\t" + "Last Time : "
+                    + dateFormat.format(u.getLastTimeUsed()));
+        }
+    }
+
+
+
 }
